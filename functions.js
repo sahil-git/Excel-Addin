2404ebff40cfba80463aaaf0800ee0662df55c63
Sahil Slide Sigma <webdevelopersahil@gmail.com>
2025-04-22 15:36:15 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,2 +1,2 @@
-Office.onReady((function(){CustomFunctions.associate("Compliant",(function(n){return"Pizza"}))}));
+Office.onReady((function(){CustomFunctions.associate("Compliant",(function(n){return"Pizza"}))}));console.log("SahilLoaded");
 //# sourceMappingURL=functions.js.map
